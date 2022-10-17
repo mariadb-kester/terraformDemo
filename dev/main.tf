fd45204eaf4f4b78c42437af1e60e4c646f64ab8
Kester Riley <kesterriley@hotmail.com>
2022-10-17 17:14:52 +0100

    Modifications to process
    Signed-off-by: Kester Riley <kesterriley@hotmail.com>

--- dev/main.tf
+++ dev/main.tf
@@ -14,3 +14,14 @@ provider "digitalocean" {
   alias = "terraform-runner"
   token = var.demo_digital_ocean_token
 }
+
+module "kdr" {
+  source = "../modules/kdrDemo"
+
+  providers = {
+    digitalocean = digitalocean.terraform-runner
+    github       = github.terraform-runner
+  }
+
+  environment = "dev"
+}
